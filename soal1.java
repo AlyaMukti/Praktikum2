e596a88c6af218c1b168abacbd735dccba50b08a
Alya Mukti Az-Zahra <145258426+AlyaMukti@users.noreply.github.com>
2023-09-27 08:40:10 +0700

    Upload file java

---
+++ soal1.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Praktikum2;
+
+import java.util.Scanner;
+/**
+ *
+ * @author LENOVO
+ */
+public class soal1 {
+    public static void main(String[] args) {
+        Scanner masukkan = new Scanner(System.in);
+        
+        System.out.println("Luas Balok");
+        System.out.print("Panjang : ");
+        String panjang = masukkan.nextLine();
+        
+        System.out.print("Lebar : ");
+        String lebar = masukkan.nextLine();
+        
+        System.out.print("Tinggi : ");
+        String tinggi = masukkan.nextLine();
+        
+        System.out.println("Luas balok adalah ");
+    }
+}
---
+++ soal3.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Praktikum2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class soal3 {
+    public static void main(String[] args) {
+        double nilai1 = 17;
+        double nilai2 = 22;
+        double nilai3 = 31;
+        double nilai4 = 43;
+        
+        System.out.println(" nilai rata-rata ");
+        double nilaiakhir = (nilai1 + nilai2 + nilai3 + nilai4)/4;
+        System.out.println("Hasil rata-rata adalah : " + nilaiakhir);
+    }
+}
